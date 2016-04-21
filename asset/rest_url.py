79515f252ef8e9967277e18e7609ebf70af3d8c3
yuyc <ownlearn@163.com>
2016-04-21 22:47:30 +0800

    着手处理post请求，完成restful跟post请求解耦

--- asset/rest_url.py
+++ asset/rest_url.py
@@ -9,7 +9,7 @@ router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'Asset', views.AssetViewSet)
 
-
+#
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
